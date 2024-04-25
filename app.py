8bddf0e2d6907fd1601f5fb49a00c831adec7a5e
kishan <kishantiwari286@gmail.com>
2024-04-25 08:22:41 +0530

    store application in database

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask , render_template
-from database import load_jobs_from_db
+from flask import Flask , render_template,request,jsonify
+from database import load_jobs_from_db,load_data_for_id,save_to_db
 app = Flask(__name__)
 
 @app.route("/")
 def first():
     job = load_jobs_from_db() 
-
     return render_template('home.html',jobs=job)
+
+@app.route("/job/<id>")
+def job_id(id):
+    data = load_data_for_id(id)
+    return render_template('jobinfo.html', data=data)
+
+# @app.route("/job/<id>/apply")     #without post method for job apply
+# def job_apply(id):
+#     form_data = request.args
+#     return form_data
+
+@app.route("/job/<id>/apply" ,methods=['post'])     #with post method for job apply
+def job_apply(id):
+    job = load_data_for_id(id)
+    form_data = request.form
+   # Convert ImmutableMultiDict to regular dictionary
+    data_dict = dict(form_data)
+    save_to_db(id,data=data_dict)
+    return render_template('application_submit.html',data=data_dict,job=job)
 if __name__ == '__main__':
     app.run(debug=True,port=8000)                  #we can change port also port = 8000 and host also host = :"0.0.0.0"
--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 import pymysql
 import os
+
 timeout = 10
+
 def get_connection():
     connection = pymysql.connect(
         charset="utf8mb4",
@@ -16,14 +18,41 @@ def get_connection():
     )
     return connection
 
-
-
 def load_jobs_from_db():
-  try:
-    connection = get_connection()
-    cursor = connection.cursor()
-    # print("connection succesfully")
-    cursor.execute("select * from jobs")
-    return cursor.fetchall()
-  finally:
-    connection.close()
+    connection = None
+    try:
+        connection = get_connection()
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM jobs")
+            return cursor.fetchall()
+    finally:
+        if connection:
+            connection.close()
+
+def load_data_for_id(id):
+    connection = None
+    try:
+        connection = get_connection()
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM jobs WHERE id = %s", (id,))
+            result = cursor.fetchall()
+            if not result:
+                return "<p>Job ID not found</p>"
+            else:
+                return result[0]
+    finally:
+        if connection:
+            connection.close()
+def save_to_db(id,data):
+    
+    connection = None
+    try:
+       connection = get_connection()
+       cursor = connection.cursor()
+       query = "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+       values = (id, data["full_name"], data["email_id"], data["linkedin_url"], data["Education"], data["work_experience"], data["resume_url"])
+       cursor.execute(query, values)
+       connection.commit()
+    finally:
+      if connection:
+        connection.close()
